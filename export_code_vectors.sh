f5250cb8a0c950b7a9ebf290d8ad9d47189fc1ad
marcus <mhagglun@gmail.com>
2021-02-07 20:17:46 +0100

    Add exporting of token and target embeddings

--- export_code_vectors.sh
+++ export_code_vectors.sh
@@ -2,4 +2,5 @@
 ###########################################################
 # Exports the code vectors for the test data
 # Pass the model name as the first argument and the dataset name as the second
-python3 code2vec.py --load models/$1/saved_model --test data/$2/$2.test.c2v --export_code_vectors
+python3 code2vec.py --load $1 --test data/$2/$2.test.c2v --export_code_vectors
+python3 code2vec.py --load $1 --save_w2v data/$2/tokens.txt --save_t2v data/$2/targets.txt
